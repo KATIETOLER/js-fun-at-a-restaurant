1008714d9dabe497a2902a1fae6d5bb0870d707a
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-12-03 15:55:07 -0700

    Add half way passing restaurant tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,39 @@
 
+function createRestaurant(name) {
+var restaurant = {
+  name: name,
+  menus: menu = {
+    breakfast: [],
+    lunch: [],
+    dinner: []
+  }
+    }
+  return restaurant
+}
+
+function addMenuItem(restaurant, food) {
+    if(food.type === "breakfast" && !restaurant.menus.breakfast.includes(food)) {
+    restaurant.menus.breakfast.push(food)
+      }
+     if(food.type === "lunch" && !restaurant.menus.lunch.includes(food)) {
+    restaurant.menus.lunch.push(food)
+      }
+    if(food.type === "dinner" && !restaurant.menus.dinner.includes(food)) {
+    restaurant.menus.dinner.push(food)
+      }
+      return restaurant
+  }
+
+function removeMenuItem(restaurant, item, type) {
+  if(type === "breakfast"){
+    restaurant.menus.breakfast.splice(item, 1)
+  }
+  return `No one is eating our ${item} - it has been removed from the ${type} menu!`
+}
+
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
