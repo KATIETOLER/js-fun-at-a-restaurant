93b3e1a3f13aadf07522c09930c28c2f8eee6c05
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-12-04 18:53:25 -0700

    Add Chef stuff

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,23 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant
+
+  }
+  greetCustomer(name, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${name}!`
+    }
+      return `Hello, ${name}!`
+  }
+  checkForFood(food) {
+    for (var i = 0; i < this.restaurant.menus[food.type].length; i++) {
+    if(this.restaurant.menus[food.type][i] === food)
+    {
+    return `Yes, we're serving ${food.name} today!`
+    }
+  }
+    return `Sorry, we aren't serving ${food.name} today.`
   }
 }
 
